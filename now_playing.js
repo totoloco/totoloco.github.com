f48054595764e0aa6e447ea5c243c8562d6de262
totoloco <totoloco en gmail>
2010-01-23 15:52:22 -0300

    playing

--- now_playing.js
+++ now_playing.js
@@ -1,4 +1,4 @@
 totoloco_now_playing([
-,{"title": "$MPD_SONG_TAG_TITLE", "artist": "$MPD_SONG_TAG_ARTIST", "album": "$MPD_SONG_TAG_ALBUM", "track": "$MPD_SONG_TAG_TRACK", "genre": "$MPD_SONG_TAG_GENRE", "date": $(date +%s)}
-,{"title": "Standing On The Rock", "artist": "GLENN HUGHES", "album": "Songs In The Key Of Rock", "track": "16", "genre": "Rock/Pop", "date": 1264272666}
+{"title": "Standing On The Rock", "artist": "GLENN HUGHES", "album": "Songs In The Key Of Rock", "track": "16", "genre": "Rock/Pop", "date": 1264272666}
+,{"title": "Said You'd Be", "artist": "", "album": "", "track": "17", "genre": "", "date": 1264272742}
 ]);
