0a0f728f998caa4ce8b7ea201ff93c4f4137e568
totoloco <totoloco en gmail>
2010-01-23 15:49:12 -0300

    playing

--- mpdcron/hooks/dump-song.sh
+++ mpdcron/hooks/dump-song.sh
@@ -5,15 +5,9 @@ gitdir=$HOME/descargas/compilando/totoloco.github.com
 lines=$(wc -l $nowplaying | sed 's/ .*//')
 sed -i ${lines}d $nowplaying
 
-echo ",{
-'title': '$MPD_SONG_TAG_TITLE',
-'artist': '$MPD_SONG_TAG_ARTIST',
-'album': '$MPD_SONG_TAG_ALBUM',
-'track': '$MPD_SONG_TAG_TRACK',
-'genre': '$MPD_SONG_TAG_GENRE',
-'date': $(date +%s)
-}
-]);" >> $nowplaying
+
+echo ',{"title": "$MPD_SONG_TAG_TITLE", "artist": "$MPD_SONG_TAG_ARTIST", "album": "$MPD_SONG_TAG_ALBUM", "track": "$MPD_SONG_TAG_TRACK", "genre": "$MPD_SONG_TAG_GENRE", "date": $(date +%s)}
+]);' >> $nowplaying
 
 cd $gitdir
 git commit -a -m 'playing'
