9150be703eb6168c5be6da0976d5f3db26c7f422
totoloco <totoloco en gmail>
2010-01-23 10:10:46 -0300

    ahora debería funca ¿?

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $.getJSON('http://github.com/totoloco/now_playing/raw/master/now_playing.js', 'totoloco_now_playing', function (data) {
+  $.getJSON('http://totoloco.github.com/now_playing.js', 'totoloco_now_playing', function (data) {
     alert(data.length);
   });
 });
