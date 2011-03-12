527187441cb0043c65f634e2db73bd7798c6b8dc
ushcompu <totoloco@gmx.com>
2011-03-12 15:56:24 -0300

    borrando tutto

--- now_playing.js
+++
@@ -1,5 +0,0 @@
-totoloco_now_playing([
-{"title": "Standing On The Rock", "artist": "GLENN HUGHES", "album": "Songs In The Key Of Rock", "track": "16", "genre": "Rock/Pop", "date": 1264272666}
-,{"title": "Said You'd Be", "artist": "", "album": "", "track": "17", "genre": "", "date": 1264272742}
-,{"title": "Said You'd Be", "artist": "", "album": "", "track": "17", "genre": "", "date": 1264272773}
-]);
